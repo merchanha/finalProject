6d5393b2d488912a22f69ee636bd06d2a5381085
henry <merchanha@gmail.com>
2021-11-02 20:32:28 -0300

    serviciosPortada

--- src/components/CoverPortada.js
+++ src/components/CoverPortada.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import instalacion from "../media/instalacion.jpg"
+import planos from "../media/planos.jpg"
+import producto from "../media/producto.jpg"
+
 
 function CoverPortada() {
     return (
@@ -7,7 +11,7 @@ function CoverPortada() {
             <div class="row row-cols-1 row-cols-md-3 g-0 p-3">
                 <div class="col">
                     <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="..." />
+                        <img src={planos} class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -19,7 +23,7 @@ function CoverPortada() {
                 </div>
                 <div class="col">
                     <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="..." />
+                        <img src={producto} class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
@@ -31,7 +35,7 @@ function CoverPortada() {
                 </div>
                 <div class="col">
                     <div class="card h-100">
-                        <img src="..." class="card-img-top" alt="..." />
+                        <img src={instalacion} class="card-img-top" alt="..." />
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
